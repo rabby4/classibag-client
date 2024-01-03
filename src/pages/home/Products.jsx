41ce4e3235610f03c920cd22b60151d5121544e6
rabby4 <mdrabbybd4747@gmail.com>
2024-01-03 23:55:14 +0600

    create inital product card

--- src/pages/home/Products.jsx
+++ src/pages/home/Products.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
-import { Container, Tab, Tabs } from 'react-bootstrap';
+import { Card, Col, Container, Row, Tab, Tabs } from 'react-bootstrap';
 
 const Products = () => {
     return (
-        <Container className='mb-5'>
+        <Container className='my-5'>
+            <h2 className='text-center display-6'>Trending Products</h2>
             <Tabs
                 defaultActiveKey="profile"
                 id="justify-tab-example"
                 className="mb-3"
                 justify
+
             >
-                <Tab eventKey="home" title="Home">
-                    Tab content for Home
+                <Tab eventKey="featured" title="Featured" active>
+                    <Row>
+                        <Col xs={12} md={8} lg={3}>
+                            <Card className='border-0'>
+                                <Card.Img variant="top" src="https://classibag-codezeel.myshopify.com/cdn/shop/files/01.jpg?v=1699246974&width=713" />
+                                <Card.Body>
+                                    <Card.Text>Gucci</Card.Text>
+                                    <Card.Title>Summer Nights Backpack Palm Trees</Card.Title>
+                                    <Card.Text>Gucci</Card.Text>
+
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                    </Row>
+
                 </Tab>
-                <Tab eventKey="profile" title="Profile">
+                <Tab eventKey="new-arrivals" title="New Arrivals">
                     Tab content for Profile
                 </Tab>
-                <Tab eventKey="longer-tab" title="Loooonger Tab">
+                <Tab eventKey="best-seller" title="Best Seller">
                     Tab content for Loooonger Tab
                 </Tab>
             </Tabs>
